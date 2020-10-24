bd2ae413b082a39b0bff61bb211a7637b2a73a43
Joe DeFranco <jdefranco449@gmail.com>
2020-10-24 10:50:23 -0400

    done

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,27 +1,53 @@
-const router = require("express").Router();
-const store = require("../db/store");
+const fs = require("fs");
+var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
-// GET "/api/notes" responds with all notes from the database
-router.get("/notes", function(req, res) {
-  store
-    .getNotes()
-    .then(notes => res.json(notes))
-    .catch(err => res.status(500).json(err));
-});
 
-router.post("/notes", (req, res) => {
-  store
-    .addNote(req.body)
-    .then((note) => res.json(note))
-    .catch(err => res.status(500).json(err));
-});
+module.exports = function(app) {
 
-// DELETE "/api/notes" deletes the note with an id equal to req.params.id
-router.delete("/notes/:id", function(req, res) {
-  store
-    .removeNote(req.params.id)
-    .then(() => res.json({ ok: true }))
-    .catch(err => res.status(500).json(err));
-});
+    app.get("/api/notes", function(req, res) {
+       
+        res.json(data);
 
-module.exports = router;
+    });
+
+    app.get("/api/notes/:id", function(req, res) {
+
+        res.json(data[Number(req.params.id)]);
+
+    });
+
+
+    app.post("/api/notes", function(req, res) {
+
+        let newNote = req.body;
+        let uniqueId = (data.length).toString();
+        console.log(uniqueId);
+        newNote.id = uniqueId;
+        data.push(newNote);
+        
+        fs.writeFileSync("./db/db.json", JSON.stringify(data), function(err) {
+            if (err) throw (err);        
+        }); 
+
+        res.json(data);    
+
+    });
+
+    
+    app.delete("/api/notes/:id", function(req, res) {
+
+        let noteId = req.params.id;
+        let newId = 0;
+        console.log(`Deleting note with id ${noteId}`);
+        data = data.filter(currentNote => {
+           return currentNote.id != noteId;
+        });
+        for (currentNote of data) {
+            currentNote.id = newId.toString();
+            newId++;
+        }
+        fs.writeFileSync("./db/db.json", JSON.stringify(data));
+        res.json(data);
+    }); 
+
+}
