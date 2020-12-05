f49930a3aa162ef50a0a21d10385f36024cacffa
Joe DeFranco <jdefranco449@gmail.com>
2020-12-05 14:47:14 -0500

    updated

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,46 +1,37 @@
-const fs = require("fs");
-var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+const router = require("express").Router();
+const store = require("../db/store");
+// var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
 
 module.exports = function(app) {
 
-    app.get("/api/notes", function(req, res) {
-       
-        res.json(data);
-
+    router.get("/notes", (req, res) => {
+        store
+            .getNotes()
+            .then((notes)=> res.json(notes))
+            .catch((err) => res.status(500).json(err));
     });
-
-    app.get("/api/notes/:id", function(req, res) {
+// fix after
+    router.get("/api/notes/:id", function(req, res) {
 
         res.json(data[Number(req.params.id)]);
 
     });
 
 
-    app.post("/api/notes", function(req, res) {
-
-        let newNote = req.body;
-        let uniqueId = (data.length).toString();
-        console.log(uniqueId);
-        newNote.id = uniqueId;
-        data.push(newNote);
-        
-        fs.writeFileSync("./db/db.json", JSON.stringify(data), function(err) {
-            if (err) throw (err);        
-        }); 
-
-        res.json(data);    
+    router.post("/notes", function(req, res) {
+        store.addNote(req.body).then((note) => res.json(note));            
 
     });
 
     
-    app.delete("/api/notes/:id", function(req, res) {
+    router.delete("/api/notes/:id", function(req, res) {
 
         let noteId = req.params.id;
         let newId = 0;
         console.log(`Deleting note with id ${noteId}`);
         data = data.filter(currentNote => {
-           return currentNote.id != noteId;
+            return currentNote.id != noteId;
         });
         for (currentNote of data) {
             currentNote.id = newId.toString();
