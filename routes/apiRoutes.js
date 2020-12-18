ae8eb2134f9f521941ef52f3e81485fbe030bd29
Joe DeFranco <jdefranco449@gmail.com>
2020-12-18 15:58:49 -0500

    fix

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,15 +3,15 @@ const store = require("../db/store");
 
 
 
-    router.get("/api/notes", (req, res) => {
+    router.get("api/notes", (req, res) => {
         store.getNotes().then((notes)=> res.json(notes)).catch((err) => res.status(500).json(err));
     });
 
-    router.post("/api/notes", function(req, res) {
+    router.post("api/notes", function(req, res) {
         store.addNote(req.body).then((note) => res.json(note)).catch((err) => res.status(500).json(err));            
     });
 
-    router.delete("/api/notes/:id", (req, res) => {
+    router.delete("api/notes/:id", (req, res) => {
         store.removeNote(rew.params.id).then(() => res.json({ ok: true })).catch((err) => res.status(500).json(err));
     });  
 
