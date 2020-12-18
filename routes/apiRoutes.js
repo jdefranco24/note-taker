9b94518ade2a5a95eb0d8511921fc22d08637ace
Joe DeFranco <jdefranco449@gmail.com>
2020-12-18 16:09:17 -0500

    yes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,16 +3,28 @@ const store = require("../db/store");
 
 
 
-    router.get("api/notes", (req, res) => {
-        store.getNotes().then((notes)=> res.json(notes)).catch((err) => res.status(500).json(err));
+    router.get("/notes", (req, res) => {
+        store
+            .getNotes()
+            .then((notes)=> res.json(notes))
+            .catch((err) => res.status(500).json(err));
     });
 
-    router.post("api/notes", function(req, res) {
-        store.addNote(req.body).then((note) => res.json(note)).catch((err) => res.status(500).json(err));            
+
+
+    router.post("/notes", function(req, res) {
+        store
+            .addNote(req.body)
+            .then((note) => res.json(note))
+            .catch((err) => res.status(500).json(err));
     });
 
-    router.delete("api/notes/:id", (req, res) => {
-        store.removeNote(rew.params.id).then(() => res.json({ ok: true })).catch((err) => res.status(500).json(err));
-    });  
+    
+    router.delete("/notes/:id", function(req, res) {
+        store
+            .removeNote(req.params.id)
+            .then(() => res.json({ ok: true }))
+            .catch((err) => res.status(500).json(err));
+    }); 
 
 module.exports = router;
